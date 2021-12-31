f2de55046595f172c48477a03e362578821bc831
Ivan Annan <primeguyy@gmail.com>
2021-12-31 17:27:28 +0000

    http: incorporating html into content

--- http/app.js
+++ http/app.js
@@ -7,11 +7,20 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   if (req.url === "/") {
-    res.end("Welcome to our homepage. This is where the journey starts...");
+    res.end(`
+        <h1>Welcome to our homepage</h1> 
+        <h3>This is where the journey starts...</h3>
+    `);
   } else if (req.url === "/about") {
-    res.end("About Us. We're the best...");
+    res.end(`
+        <h1>About Us</h1> 
+        <p>We're the best...</p>
+    `);
   } else {
-    res.end("ERROR 404... You need to be redirected");
+    res.end(`
+        <h1>ERROR 404...</h1>
+        <p>We can't find that page</p>
+     `);
   }
 });
 
