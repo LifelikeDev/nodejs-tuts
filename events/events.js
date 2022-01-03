01f57723c82685a03d41cbff97368b3bd6478567
Ivan Annan <primeguyy@gmail.com>
2022-01-03 11:57:40 +0000

    event emitters: more intro

--- events/events.js
+++ events/events.js
@@ -6,4 +6,20 @@ customEmitter.on("response", () => {
   console.log(`event emitted: data received...`);
 });
 
+customEmitter.on("response", () => {
+  console.log("another event emitted here...");
+});
+
+customEmitter.on("response", () => {
+  console.log("one more event emitted here...");
+});
+
+customEmitter.on("process", () => {
+  console.log(
+    `data successfully processed in ${new Date().getMilliseconds()} seconds`
+  );
+});
+
 customEmitter.emit("response");
+console.log("==================");
+customEmitter.emit("process");
