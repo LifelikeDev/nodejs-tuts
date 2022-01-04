206f89aaefaee3107fe2cd1664e041a6f60e152f
Ivan Annan <primeguyy@gmail.com>
2022-01-04 20:53:20 +0000

    page routing with req.url

--- express.js/app.js
+++ express.js/app.js
@@ -1,9 +1,28 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "content-type": "text/html" });
-  res.write(`<h1 style="text-align: center">hello friend...</h1>`);
-  res.end();
+  const url = req.url;
+
+  if (url === "/") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write(`<h1 style="text-align: center">Home page...</h1>`);
+    res.end();
+  } else if (url === "/about") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write(`<h1 style="text-align: center">About Us...</h1>`);
+    res.end();
+  } else {
+    res.writeHead(404, { "content-type": "text/html" });
+    res.write(`
+        <h3 style="text-align: center">
+        Resource Not Found <br>
+        <a href="/">Back home</a>
+        </h3>
+        
+    `);
+    res.end();
+  }
+
   console.log("response received by user");
 });
 
