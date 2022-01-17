0e9168b804ff78b4bd2a18b6f4c9e107d14c4dc4
dev-navian <ivan.annan@turntabl.io>
2022-01-17 20:54:57 +0000

    serving up an html file for display

--- express.js/app.js
+++ express.js/app.js
@@ -1,11 +1,14 @@
 const http = require("http");
+const { readFileSync } = require("fs");
+
+const homepage = readFileSync("../nodejs-tuts/express.js/index.html");
 
 const server = http.createServer((req, res) => {
   const url = req.url;
 
   if (url === "/") {
     res.writeHead(200, { "content-type": "text/html" });
-    res.write(`<h1 style="text-align: center">Home page...</h1>`);
+    res.write(homepage);
     res.end();
   } else if (url === "/about") {
     res.writeHead(200, { "content-type": "text/html" });
