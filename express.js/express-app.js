dbc06e8f4c1589efcfb94084b787e2ba53515334
Ivan Annan <primeguyy@gmail.com>
2022-01-19 21:47:23 +0000

    modified html template displayed from server

--- express.js/express-app.js
+++ express.js/express-app.js
@@ -3,7 +3,7 @@ const app = express();
 const portNumber = 4000;
 
 app.get("/", (req, res) => {
-  res.send(`
+  res.status(200).send(`
     <main style="text-align: center; padding-top: 1rem;">
     <h1>Welcome to the express server</h1> 
     <h3 >Homepage</h3>
@@ -13,21 +13,24 @@ app.get("/", (req, res) => {
 });
 
 app.get("/about", (req, res) => {
-  res.send(`
+  res.status(200).send(`
     <main style="text-align: center; padding-top: 1rem;">
     <h1>Welcome to the express server</h1> 
-    <h3 >About page</h3>
-    <p><a href="/">Homepage</a></p>
+    <h3>About page</h3>
+    <p>
+        <a href="/" style="margin-right: 0.5rem;">Homepage</a>
+        <a href="/error">Error page</a>
+    </p>
     </main>
     `);
 });
 
 app.all("*", (req, res) => {
-  res.send(`
+  res.status(404).send(`
     <main style="text-align: center; padding-top: 1rem;">
     <h2>Error 404</h2>
-    <h2>Wrong destination</h2>
-    <p>Redirect to <a href="/">homepage</a></p>
+    <h2>Resource not found</h2>
+    <p>Redirect to <a href="/" style="margin-left: 0.25rem;">homepage</a></p>
     </main>
     `);
 });
