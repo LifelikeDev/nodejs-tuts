507f2882dcb2e533868a6cfcf62c577a1303f958
Ivan Annan <primeguyy@gmail.com>
2022-02-08 22:46:10 +0000

    serving api in json

--- express.js/api-dev/app.js
+++ express.js/api-dev/app.js
@@ -1,19 +1,18 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const { people, bios } = require("./data");
 
 app.get("/", (req, res) => {
-  // res.send(
-  //   `<h1 style="text-align: center;">Welcome to API Development with Express.js</h2>`
-  // );
+  // res.json([
+  //   { name: "Ontario", country: "USA" },
+  //   { name: "Croydon", country: "UK" },
+  //   { name: "Berlin", country: "Germany" },
+  //   { name: "Shanghai", country: "China" },
+  //   { name: "Johannesburg", country: "South Africa" },
+  // ]);
 
-  res.json([
-    { name: "Ontario", country: "USA" },
-    { name: "Croydon", country: "UK" },
-    { name: "Berlin", country: "Germany" },
-    { name: "Shanghai", country: "China" },
-    { name: "Johannesburg", country: "South Africa" },
-  ]);
+  res.json(bios);
 });
 
 app.listen(port, () => {
---
+++ express.js/api-dev/data.js
@@ -0,0 +1,42 @@
+const people = [
+  { id: 1, name: "Matthew" },
+  { id: 2, name: "Luke" },
+  { id: 3, name: "John" },
+  { id: 4, name: "Mark" },
+  { id: 5, name: "Peter" },
+];
+
+const bios = [
+  {
+    id: 1,
+    name: "Thomas",
+    title: "UX Designer",
+    description: "loyal, friendly, kind",
+  },
+  {
+    id: 2,
+    name: "Jean",
+    title: "UI Designer",
+    description: "creative, explorative, proactive",
+  },
+  {
+    id: 3,
+    name: "Andrew",
+    title: "React Developer",
+    description: "cool, energetic, optimistic",
+  },
+  {
+    id: 4,
+    name: "Sarah",
+    title: "Web3 / BlockChain Developer",
+    description: "smart, peaceful, sociable",
+  },
+  {
+    id: 5,
+    name: "Kwame",
+    title: "Svelte Developer",
+    description: "playful, responsive, admirable",
+  },
+];
+
+module.exports = { people, bios };
