75cac6669896a72b498dd2a91ffd0d3bc7438d8d
Ivan Annan <primeguyy@gmail.com>
2022-02-15 22:08:42 +0000

    http post method with form data parse

--- express.js/post-method/app.js
+++ express.js/post-method/app.js
@@ -5,8 +5,17 @@ const path = require("path");
 
 app.use(express.static(path.join(__dirname, "methods-public")));
 
+// parse form data
+app.use(express.urlencoded({ extended: false }));
+
 app.get("/another", (req, res) => {
   res.send(`Hello friend... Welcome!!!`);
+});
+
+app.post("/login", (req, res) => {
+  const { name } = req.body;
+
+  res.send(`<h2>Welcome, ${name}</h2>`);
 });
 
 app.listen(portNumber, () => {
