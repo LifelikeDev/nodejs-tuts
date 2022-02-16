393eee9c8a1eb45fca0aef8d1aa81d89eaea202a
Ivan Annan <primeguyy@gmail.com>
2022-02-16 22:19:14 +0000

    post form data

--- express.js/post-method/app.js
+++ express.js/post-method/app.js
@@ -9,8 +9,23 @@ app.use(express.static(path.join(__dirname, "methods-public")));
 // parse form data
 app.use(express.urlencoded({ extended: false }));
 
+// parse json
+app.use(express.json());
+
 app.get("/api/people", (req, res) => {
   res.status(200).json({ success: true, data: people });
+});
+
+app.post("/api/people", (req, res) => {
+  const { name } = req.body;
+
+  if (!name) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Please enter a correct name..." });
+  }
+
+  return res.status(201).json({ success: true, person: name });
 });
 
 app.get("/another", (req, res) => {
