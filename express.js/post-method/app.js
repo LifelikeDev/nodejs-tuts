4dfa2baf20fd29bb8e5f1b4dbd8aa24c7a188eb3
Ivan Annan <primeguyy@gmail.com>
2022-02-17 22:55:06 +0000

    put http request

--- express.js/post-method/app.js
+++ express.js/post-method/app.js
@@ -46,6 +46,28 @@ app.post("/login", (req, res) => {
   res.status(200).send(`<h2>Welcome, ${name}</h2>`);
 });
 
+app.put("/api/people/:id", (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  const person = people.find((p) => p.id === Number(id));
+
+  if (!person) {
+    res
+      .status(404)
+      .json({ success: false, message: `Person with ID ${id} not found...` });
+  }
+
+  const updatedPeople = people.map((person) => {
+    if (person.id === Number(id)) {
+      person.name = name;
+    }
+    return person;
+  });
+
+  res.status(200).json({ success: true, data: person });
+});
+
 app.listen(portNumber, () => {
   console.log(
     `Server is running successfully on http://localhost:${portNumber}`
